3fc1468380ceec77237273610eec42779c955b22
Its-Delimas <spencerdelimas@gmail.com>
2026-06-09 00:28:55 +0300

    feat: module-02-params

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,23 @@ app.get('/',(req:Request,res:Response)=>{
     res.json({message:"NexLink API is running"})
 })
 
+//Query string - fetch all links with optional filters
+app.get('/links',(req:Request,res:Response)=>{
+    const {limit, active} = req.query
+    res.json({
+        message:'Fetching all links',
+        limit: limit ? Number(limit):10,
+        active:active==='true'
+    })
+})
+
+//Route params ` fetch singlelink by its short code
+app.get('/links/:code',(req:Request,res:Response)=>{
+    const {code} = req.params
+    res.json({message:"Fetching link",code})
+})
+
+
 app.listen(PORT,()=>{
     console.log(`Server running on http://localhost:${PORT}`)
 })
